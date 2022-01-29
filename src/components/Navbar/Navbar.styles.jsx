51bcbc0f72f2bb2cca98f9c5ab381d7fe4dc3e48
Vllaaaddd <mr.trompak3@gmail.com>
2022-01-29 20:22:47 +0200

    zalupa

---
+++ src/components/Navbar/Navbar.styles.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 50px;
+  background-color: black;
+`;
+
+export const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Left = styled.div``;
+
+export const Center = styled.div``;
+
+export const Right = styled.div``;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Center, Container, Left, Right, Wrapper } from "./Navbar.styles";
+import { LoginOutlined } from "@ant-design/icons/lib/icons";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left></Left>
+        <Center></Center>
+        <Right>
+          <LoginOutlined style={}/>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
